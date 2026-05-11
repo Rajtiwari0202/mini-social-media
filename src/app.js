8aa3b76b69e492fd58bb7bd6165d5617ca19b71b
Raj Tiwari <rajtiwari16916@gmail.com>
2026-05-11 23:06:48 +0000

    Setup image upload backend with multer and ImageKit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const multer = require("multer")
+
+const app = express();
+app.use(express.json());
+
+const upload = multer({storage: multer.memoryStorage() })
+
+app.post(
+    "/create-post",
+    upload.single("image"),
+    async (req, res) => {
+
+        console.log(req.file);
+        console.log(req.body);
+
+        res.json({
+            message: "Post created"
+        });
+    }
+);
+
+module.exports = app;
---
+++ src/models/post.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+    {
+        image: {
+            type: String,
+            required: true,
+        },
+
+        caption: {
+            type: String,
+            required: true,
+            trim: true,
+            maxlength: 200,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const postModel = mongoose.model("Post", postSchema);
+
+module.exports = postModel;
